017416b8dd559af3b2811f72196557f6b7d74a77
elboraki <younes.elboraki@gmail.com>
2020-11-02 22:54:35 +0100

    build linear serch

--- cplus/LinearSearch/main.cpp
+++ cplus/LinearSearch/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
 using namespace std;
+void linearSearch(int[], int, int);
+int main() {
+ // cout << "Hello World!" << endl;
+  int arr[] = {1, 4, 3, 2, 9, 88, 7, 5, 23};
+  int length = sizeof(arr) / sizeof(*arr);
+ // cout << 'The number is found--->' + length << endl;
+  linearSearch(arr, 88, length);
+  return 0;
+}
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+void linearSearch(int arr[], int number, int length) {
+
+  for (int i = 0; i < length; i++) {
+    if (arr[i] == number) {
+      cout << "The number is found" << endl;
+      return;
+    }
+  }
+  cout << "Not Found" << endl;
 }
